a0a9738aa23099a06db68aff633b16bb358faaac
elvina putri olivia <elvinavia123@gmail.com>
2025-09-03 20:38:00 +0700

    Update events.ts

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,31 +1,31 @@
 export const events = [
   {
     name: "Lomba",
-    date: "2025-08-10",
+    date: "10-08-2025",
     description: "Lomba di RW 05 yaitu ada lomba anak-anak,ibu,bapak, dan anggota karang taruna, dan ada lomba seluruh RT.",
     image: "/lomba.jpg",
   },
   {
     name: "Barikan",
-    date: "2025-08-16",
+    date: "16-08-2025",
     description: "Tradisi tasyakuran dan doa bersama warga RW 05 untuk menjaga kerukunan. dan ada lomba Tumpeng per-RT.",
     image: "/barikan.jpg",
   },
   {
     name: "Upacara Kemerdekaan",
-    date: "2025-08-17",
+    date: "17-08-2025",
     description: "Pengibaran bendera merah putih dalam rangka memperingati HUT RI untuk warga RW 05.",
     image: "/upacara.jpg",
   },
   {
     name: "Gerak Jalan",
-    date: "2025-08-24",
+    date: "24-08-2025",
     description: "Kegiatan jalan sehat bersama warga untuk mempererat kebersamaan.",
     image: "/gerak-jalan.jpg",
   },
   {
     name: "Pensi",
-    date: "2025-08-24",
+    date: "24-08-2025",
     description: "Ajang kreativitas pemuda menampilkan bakat seni musik, tari, dan ada penampilan dari adik-adik,bapak-bapak,ibu-ibu,anggota karang taruna.",
     image: "/pensi.jpg",
   },
