032531ef995e640f593eba1cfebdb2ed07b5d727
elvina putri olivia <elvinavia123@gmail.com>
2025-09-03 08:32:32 +0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,11 @@
 export default function AboutPage() {
   return (
-    <section className="space-y-6 p-6 bg-white rounded-xl shadow-md">
+    <section className="space-y-6 p-6 bg-green rounded-xl shadow-md">
       <h2 className="text-3xl font-bold text-blue-700 border-b-2 border-blue-300 pb-2">
         Tentang Karang Taruna "PANCASONA"
       </h2>
       
-      <p className="leading-relaxed text-gray-700">
+      <p className="leading-relaxed text-white-700">
         Karang Taruna RW 05 <strong>PANCASONA</strong> adalah kepemudaan yang dibentuk oleh masyarakat
         sebagai wadah generasi muda untuk mengembangkan diri, tumbuh, dan berkembang atas dasar kesadaran
         serta tanggung jawab sosial untuk generasi muda, yang berorientasi pada tercapainya kesejahteraan
@@ -13,18 +13,18 @@ export default function AboutPage() {
       </p>
       <br />
 
-      <p className="leading-relaxed text-gray-700">
+      <p className="leading-relaxed text-white-700">
         "PANCASONA" Di tengah hiruk, para pemuda RW 05 Lowokwaru, bangkit dengan kompak dan semangat membara
         serta tak akan pernah padam. Bukan hanya pewaris, kami adalah pejuang harapan, dengan hati yang tulus,
         menjalin persatuan dan impian. Pesona Pemuda yang tak pernah pudar karena kami adalah cahaya di tengah zaman.
       </p>
         <br />
         
-      <h3 className="text-2xl font-semibold text-gray-800">Visi</h3>
-      <p className="text-gray-700">Menjadi organisasi pemuda yang solid, kreatif, dan bermanfaat bagi masyarakat sekitar.</p>
+      <h3 className="text-2xl font-semibold text-black-800">Visi</h3>
+      <p className="text-white-700">Menjadi organisasi pemuda yang solid, kreatif, dan bermanfaat bagi masyarakat sekitar.</p>
 
-      <h3 className="text-2xl font-semibold text-gray-800">Misi</h3>
-      <ul className="list-disc list-inside space-y-2 text-gray-700">
+      <h3 className="text-2xl font-semibold text-black-800">Misi</h3>
+      <ul className="list-disc list-inside space-y-2 text-white-700">
         <li>Mengadakan kegiatan sosial,dan lingkungan.</li>
         <li>Mendorong kreativitas generasi muda.</li>
         <li>Menjadi wadah aspirasi pemuda dan masyarakat.</li>
